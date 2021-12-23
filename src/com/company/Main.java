6aafd77694505e6f6acab52ddcc9604004664a59
Ronny <ronny761@hityash.org>
2021-12-23 11:44:26 +0200

    useful methods to work with nodes

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        Node<Integer> chain = new Node<Integer>(13,
+                new Node<Integer>(9,
+                        new Node<Integer>(-2,
+                                new Node<Integer>(100,
+                                        new Node<Integer>(188)));
+
+
+
+    }
+}
