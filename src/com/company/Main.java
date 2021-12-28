16235a0cbeb59879cc63c281aca27c72faa0e3e8
Ronny <ronny761@hityash.org>
2021-12-28 13:54:40 +0200

    סדרת הפרשים

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,93 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
-        Node<Integer> chain = new Node<Integer>(-12,
-                new Node<Integer>(-9,
-                        new Node<Integer>(12,
-                                new Node<Integer>(100,
-                                        new Node<Integer>(188)))));
-
-
-
+        Node<Integer> l1 = new Node<Integer>(8,
+                new Node<Integer>(12,
+                        new Node<Integer>(40,
+                                new Node<Integer>(52,
+                                        new Node<Integer>(77,
+                                                new Node<Integer>(6,
+                                                        new Node<Integer>(165)))))));
+        Node<Integer> l2 = new Node<Integer>(1,
+                new Node<Integer>(10,
+                        new Node<Integer>(77,
+                                new Node<Integer>(12,
+                                        new Node<Integer>(165,
+                                                new Node<Integer>(52,
+                                                        new Node<Integer>(500)))))));
+        //System.out.println(isAsc(l1));
+        printChain(diffList(l1,l2));
     }
 
+    /**
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static Node<Integer> diffList(Node<Integer> l1,
+                                         Node<Integer> l2){
+        Node<Integer> tmp1 = l1;
+        Node<Integer> tmp2 = l2;
+
+        //fishy solution
+        Node<Integer> l3 = new Node<>(Integer.MAX_VALUE);
+        Node<Integer> tmp3 = l3;
+
+        while(tmp1 != null){
+            int val = tmp1.getValue();
+            if(!isExist(tmp2, val)){
+                tmp3.setNext(new Node<>(val));
+                tmp3 = tmp3.getNext();
+            }
+            tmp1 = tmp1.getNext();
+        }
+        return l3.getNext();
+    }
+
+    /**
+     *
+     * @param p
+     * @param value
+     * @return true if value is in chain
+     */
+    public static boolean isExist(Node<Integer> p, int value){
+        Node<Integer> tmp = p;
+        while(tmp.hasNext()){
+            if(tmp.getValue() == value)
+                return true;
+            tmp = tmp.getNext();
+        }
+        return false;
+    }
+    /**
+     * lesson 28/12
+     * exercise from presentation
+     * check if a given chain is sorted in asc order
+     * ...add the missing comments structure
+     */
+    public static boolean isAsc(Node<Integer> p){
+        boolean sorted = true;
+        Node<Integer> tmp = p;
+        Node<Integer> nxtTmp = p.getNext();
+
+        while(nxtTmp != null){
+            if(tmp.getValue() >= nxtTmp.getValue())
+                sorted = false;
+            nxtTmp = nxtTmp.getNext();
+            tmp = tmp.getNext();
+        }
+        return sorted;
+    }
+    /**
+     * add the node to the correct place in the sorted chain
+     * @param chain - a sorted chain of integers
+     * @param nd
+     */
+    public static void addToSortList(Node<Integer> chain,
+                                     Node<Integer> nd){
+        Node<Integer> tmp = chain;
+    }
     /**
      * adds the new node to the of the chain
      * @param chain
@@ -40,6 +115,11 @@ public class Main {
         nwNode.setNext(chain);
         chain = nwNode;
     }
+
+    /**
+     * prints the given chain
+     * @param chain
+     */
     public static void printChain(Node<Integer> chain){
         while (chain != null) {
             System.out.print(chain.getValue() +" -> ");
